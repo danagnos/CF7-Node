e8afd152dd0371f0c9f9955c48533c3cf55201f7
D. Anagnostopoulos <dimitris_anagn@hotmail.com>
2025-03-28 21:07:43 +0200

    users app controller, routes, models

--- myapp2/app.js
+++ myapp2/app.js
@@ -7,11 +7,20 @@ app.use(express.urlencoded({extended:true}))
 
 app.use('/', express.static('files'));
 
+const logger = (req, res, next) => {
+  let url = req.url;
+  console.log("Logger", req.body);
+  let time = new Date();
+  console.log('Received requests for ' + url + ' at ' + time);
+
+  next()
+}
+
 app.get('/', (req, res)=>{
   res.send("This is the home page");
 });
 
-app.post('/user', (req, res) => {
+app.post('/user', logger, (req, res) => {
   let data = req.body;
   let username = req.body.username;
   let email = req.body.email;
@@ -35,6 +44,11 @@ app.use('/user1', (req, res)=>{
 app.use('/user2', (req, res)=>{
   console.log("User 2")
   res.send("User 2 Page");
+});
+ 
+app.use('/user2/hello', (req, res)=>{
+  console.log("User 2 Hello")
+  res.send("User 2 Page Hello");
 });
 
 app.listen(port, ()=>{
