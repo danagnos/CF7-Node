e6b8b84c46741dd74a4a983e065f13bff9110783
D. Anagnostopoulos <dimitris_anagn@hotmail.com>
2025-04-02 21:06:58 +0300

    usersApp swagger, auth, jwt

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const app = express();
+const app = express()
 
 app.use(express.json());
 
-const user = require('./routes/user.routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
+const user = require('./routes/user.routes');
+const userProduct = require('./routes/user.products.routes')
+const auth = require('./routes/auth.routes');
+
+app.use('/api/auth', auth);
 app.use('/api/users', user);
+app.use('/api/user-product', userProduct);
+
+app.use(
+  '/api-docs', 
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument.options)  
+);
 
 module.exports = app
